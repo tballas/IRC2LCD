2a41beb61c3e40d980450d2152784fd502f3695e
tballas <timballas@gmail.com>
2014-05-27 00:57:49 -0700

    Might fix comments

--- Python/IRC2LCD.py
+++ Python/IRC2LCD.py
@@ -34,16 +34,16 @@ class IRC2LCDbot(irc.bot.SingleServerIRCBot):
 		c.join(self.channel)
 
 	def on_pubmsg(self, c, e):
-		pubmsgTemp = e.arguments[0]														# e.arguments[0] is the public message we are processing, loaded into "pubmsgTemp"
+		pubmsgTemp = e.arguments[0]	# e.arguments[0] is the public message we are processing, loaded into "pubmsgTemp"
 		pattern = re.compile(r'(.*{0}\s.*|.*{1}:.*)'.format(MonitorNick,MonitorNick))	# Compile Regular Expression to check if the public message has our MonitorNick in it
-		result = re.search(pattern, pubmsgTemp)											# Execute Regular Expression
-		if result:																		# Check to see if we matched our MonitorNick in the public message
-			try:																		# Handle error when result has 'None' in it
-				print result.group(1)													# Print matched message to the console
-				MatchedMessage = str(result.group(1))									# Load matched message into "MatchedMessage" variable. Enclosing it in "str()" is to return a nice printable string.
-				ser.write("\r\t" + MatchedMessage)										# Write "MatchedMessage" to LCD through Parallax Propeller over Serial connection. "\r\t" is command for Propeller to Clear LCD.
-			except:																		# Needed to complete 'try:' statement
-				pass																	# Do nothing and move on
+		result = re.search(pattern, pubmsgTemp)	# Execute Regular Expression
+		if result:	# Check to see if we matched our MonitorNick in the public message
+			try:	# Handle error when result has 'None' in it
+				print result.group(1)	# Print matched message to the console
+				MatchedMessage = str(result.group(1))	# Load matched message into "MatchedMessage" variable. Enclosing it in "str()" is to return a nice printable string.
+				ser.write("\r\t" + MatchedMessage)	# Write "MatchedMessage" to LCD through Parallax Propeller over Serial connection. "\r\t" is command for Propeller to Clear LCD.
+			except:	# Needed to complete 'try:' statement
+				pass	# Do nothing and move on
 
 def main():
 	import sys
@@ -65,21 +65,21 @@ def main():
 	nickname = sys.argv[3]
 	COMport = sys.argv[4]
 	
-	global BotNick												# Declare global variable for "BotNick"
-	if len(sys.argv) == 6:										# If there is a argument defined for "BotNick"
-		BotNick = sys.argv[5]									# Set "BotNick" to Argument 5(sys.argv[5])
-	else:														# Else
-		BotNick = nickname + "_"								# Use nickname to monitor and an underscore
+	global BotNick	# Declare global variable for "BotNick"
+	if len(sys.argv) == 6:	# If there is a argument defined for "BotNick"
+		BotNick = sys.argv[5]	# Set "BotNick" to Argument 5(sys.argv[5])
+	else:	# Else
+		BotNick = nickname + "_"	# Use nickname to monitor and an underscore
 	
-	global MonitorNick											# Declare global variable for "MonitorNick"
-	MonitorNick = nickname										# Set "MonitorNick" to nickname(sys.argv[3])
+	global MonitorNick	# Declare global variable for "MonitorNick"
+	MonitorNick = nickname	# Set "MonitorNick" to nickname(sys.argv[3])
 	
-	global ser													# Declare global variable for "ser"
-	ser = serial.Serial(str(COMport),baudrate=9600)				# Set "ser" to Serial object
+	global ser	# Declare global variable for "ser"
+	ser = serial.Serial(str(COMport),baudrate=9600)	# Set "ser" to Serial object
 	
-	bot = IRC2LCDbot(channel, nickname, server, port)			# Set "bot" to IRC2LCDbot object
-	bot.start()													# Start bot
-	ser.close()													# Closing Serial port will prevent problems
+	bot = IRC2LCDbot(channel, nickname, server, port)	# Set "bot" to IRC2LCDbot object
+	bot.start()	# Start bot
+	ser.close()	# Closing Serial port will prevent problems
 
 if __name__ == "__main__":
 	main()
